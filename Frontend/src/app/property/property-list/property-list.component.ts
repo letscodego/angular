786c6d48f637b84514ae6426f26e0d7441075680
amin <amhassanlou@gmail.com>
2021-08-29 16:04:22 -0700

    fix icon issue

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -7,6 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+
+  Properties: Array<any> = [{
+    "Id": 1,
+    "Name": "Villa",
+    "Type": "Apartment",
+    "Price": 1000
+  },
+  {
+    "Id": 2,
+    "Name": "Anegelos",
+    "Type": "House",
+    "Price": 14000
+  },
+  {
+    "Id": 3,
+    "Name": "Cali",
+    "Type": "Apartment",
+    "Price": 5000
+  },
+  {
+    "Id": 4,
+    "Name": "Van",
+    "Type": "Apartment",
+    "Price": 7000
+  },
+  {
+    "Id": 5,
+    "Name": "Coq",
+    "Type": "Apartment",
+    "Price": 6000
+  }];
+
   constructor() { }
 
   ngOnInit(): void {
